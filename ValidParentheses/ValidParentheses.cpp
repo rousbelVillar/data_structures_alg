aef2d88d80c28b7501db5f712857d9ac682bf865
rousbelVillar <rousbel@github.com>
2026-01-03 19:15:15 -0800

    add comments valid parentheses

--- ValidParentheses/ValidParentheses.cpp
+++ ValidParentheses/ValidParentheses.cpp
@@ -3,17 +3,25 @@
 #include <stack>
 
  bool ValidParentheses::isValid(std::string s) {
+     //I'm using the stack approach for this problem
      std::stack <char> st;
 
      for (int i=0; i<s.length();i++){
+         //Only push opening parentheses to the stack
          if(s.at(i) == '(' || s.at(i) == '{' || s.at(i) == '['){
              st.push(s.at(i));
          } else{
+             //If we are here that means we have reacjed a closing parentheses
+             //If our stack is empty there's no point on continuing we return false
              if(st.empty()){
                  return false;
              }
+             //Track the top of the stack before deleting it
              char top = st.top();
              st.pop();
+             //If we have a stack element and we have reached a closing parentheses that means
+             //there has to be a match for the open p at the top of the stack
+             //otherwise we have to return false.
              if(s.at(i) == ')' && top != '(' ||
                 s.at(i) == '}' && top != '{' ||
                 s.at(i) == ']' && top != '['
