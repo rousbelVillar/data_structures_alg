28644a94bab5869084f1fe8978c5ecedd492742a
rousbelVillar <rousbel@github.com>
2026-01-07 10:57:03 -0800

    optimized

--- BaseballGame/BaseballGame.cpp
+++ BaseballGame/BaseballGame.cpp
@@ -16,17 +16,15 @@ int BaseballGame::calpoints(vector<string> &operations) {
     vector<int> scores;
 
     for (string op : operations) {
-        if (op == "+" || op == "D" || op == "C") {
-            if (op == "+") {
-                int val1 = scores[scores.size()-1];
-                int val2 = scores[scores.size()-2];
-                scores.push_back(val1 + val2);
-            } else if (op == "D") {
-                int val1 = scores[scores.size()-1] * 2;
-                scores.push_back(val1);
-            }else if (op == "C") {
-                scores.pop_back();
-            }
+        if (op == "+") {
+            int val1 = scores[scores.size()-1];
+            int val2 = scores[scores.size()-2];
+            scores.push_back(val1 + val2);
+        } else if (op == "D") {
+            int val1 = scores[scores.size()-1] * 2;
+            scores.push_back(val1);
+        }else if (op == "C") {
+            scores.pop_back();
         }
         else {
             scores.push_back(stoi(op));
